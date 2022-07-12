736283c9e24defd4a9022c350d2cbc64c2cdfd70
onytianarazafy <89404360+onytianarazafy@users.noreply.github.com>
2022-07-12 21:22:08 +0300

    Create gestionParking.php

---
+++ gestionParking.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords"
+        content="wrappixel, admin dashboard, html css dashboard, web dashboard, bootstrap 5 admin, bootstrap 5, css3 dashboard, bootstrap 5 dashboard, Monsterlite admin bootstrap 5 dashboard, frontend, responsive bootstrap 5 admin template, Monster admin lite design, Monster admin lite dashboard bootstrap 5 dashboard template">
+    <meta name="description"
+        content="Monster Lite is powerful and clean admin dashboard template, inpired from Bootstrap Framework">
+    <meta name="robots" content="noindex,nofollow">
+    <title>Monster Lite Template by WrapPixel</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo site_url('assets/admin/images/favicon.png')?>">
+    <link href="<?php echo site_url('assets/admin/css/style.min.css')?>" rel="stylesheet">
+</head>
+
+<body>
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full"
+        data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+        <header class="topbar" data-navbarbg="skin6">
+            <nav class="navbar top-navbar navbar-expand-md navbar-dark">
+                <div class="navbar-header" data-logobg="skin6">
+                    <a class="navbar-brand" href="index.html">
+                        <b class="logo-icon">
+                            <img src="<?php echo site_url('assets/admin/images/logo-icon.png')?>" alt="homepage" class="dark-logo" />
+                        </b>
+                        <span class="logo-text">
+                            <img src="<?php echo site_url('assets/admin/images/logo-text.png')?>" alt="homepage" class="dark-logo" />
+                        </span>
+                    </a>
+                    <a class="nav-toggler waves-effect waves-light text-dark d-block d-md-none"
+                        href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
+                </div>
+                <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5">
+                    <ul class="navbar-nav me-auto mt-md-0 ">
+                        <li class="nav-item hidden-sm-down">
+                            <form class="app-search ps-3">
+                                <input type="text" class="form-control" placeholder="Search for..."> <a
+                                    class="srh-btn"><i class="ti-search"></i></a>
+                            </form>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle waves-effect waves-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                               
+                            </a>
+                            <ul class="dropdown-menu show" aria-labelledby="navbarDropdown"></ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <aside class="left-sidebar" data-sidebarbg="skin6">
+            <div class="scroll-sidebar">
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="index.html" aria-expanded="false"><i class="me-3 far fa-clock fa-fw"
+                                    aria-hidden="true"></i><span class="hide-menu">Retour</span></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+        <div class="page-wrapper">
+            <div class="page-breadcrumb">
+                <div class="row align-items-center">
+                    <div class="col-md-6 col-8 align-self-center">
+                        <h3 class="page-title mb-0 p-0">Add Place de Vehicule</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-4 col-xlg-3 col-md-5">     
+                    </div>
+                    <div class="col-lg-8 col-xlg-9 col-md-7">
+                        <div class="card">
+                            <div class="card-body">
+                                <form action="<?php echo site_url()?>Parking/AjouterPlaceParking" method="post" class="form-horizontal form-material mx-2">
+                                <div class="form-group">
+                                    <div class="form-group">
+                                        <label class="col-md-12 mb-0"> Numero Parking</label>
+                                        <div class="col-md-12">
+                                            <input type="number" name="numero"
+                                                class="form-control ps-0 form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12 mb-0"> Remarque</label>
+                                        <div class="col-md-12">
+                                            <input type="text" name="remarque"
+                                                class="form-control ps-0 form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12 mb-0"> Taille</label>
+                                        <div class="col-md-12">
+                                            <input type="number" name="taille"
+                                                class="form-control ps-0 form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-sm-12 d-flex">
+                                            <button class="btn btn-success mx-auto mx-md-0 text-white">Add
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer class="footer text-center">
+                © 2021 Monster Admin by <a href="https://www.wrappixel.com/">wrappixel.com</a>
+            </footer>
+        </div>
+    </div>
+    <script src="<?php echo site_url('assets/admin/plugins/jquery/dist/jquery.min.js')?>"></script>
+    <script src="<?php echo site_url('assets/admin/plugins/bootstrap/dist/js/bootstrap.bundle.min.js')?>"></script>
+    <script src="<?php echo site_url('assets/admin/js/app-style-switcher.js')?>"></script>
+    <script src="<?php echo site_url('assets/admin/js/waves.js')?>"></script>
+    <script src="<?php echo site_url('assets/admin/js/sidebarmenu.js')?>"></script>
+    <script src="<?php echo site_url('assets/admin/js/custom.js')?>"></script>
+</body>
+
+</html>
